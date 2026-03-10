e5bd24dc75432fac311477ff3b4f62d44f572b99
gnaneswar944 <gk7408@srmist.edu.in>
2026-03-10 09:46:01 +0530

    UC2:Basic room type and static availability

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,5 +1,56 @@
+ abstract class Room{
+    protected int numberOfBeds;
+    protected int squareFeet;
+    protected double pricePerNight;
+    public Room(int numberofBeds,int squareFeet,double pricePerNight){
+        this.numberOfBeds = numberofBeds;
+        this.squareFeet = squareFeet;
+        this.pricePerNight = pricePerNight;
+    }
+    public void displayRoomDetails(){
+        System.out.println("Beds :"+numberOfBeds);
+        System.out.println("size :"+squareFeet+"spft");
+        System.out.println("price per night"+pricePerNight);
+    }
+}
+class SingleRoom extends Room{
+    public SingleRoom(){
+        super(1,250,1500.0);
+    }
+}
+class DoubleRoom extends Room{
+    public DoubleRoom(){
+        super(2,400,2500.0);
+    }
+}
+ class SuiteRoom extends Room{
+    public SuiteRoom(){
+        super(3,750,5000.0);
+    }
+}
 public class BookMyStayApp {
     public static void main(String[] args) {
-    System.out.println("welcome to hotel management system");
+    Room Single = new SingleRoom();
+    Room doubleRoom = new DoubleRoom();
+    Room suite = new SuiteRoom();
+
+        int singleAvailable = 5;
+        int doubleAvailable = 3;
+        int suiteAvailable = 2;
+
+        System.out.println("Hotel Room Initialization\n");
+
+        System.out.println("Single Room:");
+        Single.displayRoomDetails();
+        System.out.println("Available: " + singleAvailable);
+
+        System.out.println("\nDouble Room:");
+        doubleRoom.displayRoomDetails();
+        System.out.println("Available: " + doubleAvailable);
+
+        System.out.println("\nSuite Room:");
+        suite.displayRoomDetails();
+        System.out.println("Available: " + suiteAvailable);
+
     }
 }
